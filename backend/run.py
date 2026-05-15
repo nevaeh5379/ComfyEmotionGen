61422a2ee0afb1c79a57d327bbd5069d70ff2a30
nevaeh5379 <230626330+nevaeh5379@users.noreply.github.com>
2026-05-15 23:47:23 +0900

    feat: uvicorn 서버 실행 방식을 비동기로 변경

--- backend/run.py
+++ backend/run.py
@@ -6,4 +6,12 @@ if sys.platform == "win32":
 
 import uvicorn
 
-uvicorn.run("server:app", port=8000, log_level="info")
+
+async def main():
+    config = uvicorn.Config("server:app", host="127.0.0.1", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
